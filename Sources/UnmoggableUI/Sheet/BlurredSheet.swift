7df3c4334137bc3e5139e77fb246090b7391ee66
Lorycara <lorycara62@gmail.com>
2025-01-28 18:37:53 +0100

    [Feature[ Added blurred sheets and PersistentBlurredBottomSheets

---
+++ Sources/UnmoggableUI/Sheet/BlurredSheet.swift
@@ -0,0 +1,61 @@
+//
+//  BlurredSheet.swift
+//  Hairmax
+//
+//  Created by Lorenzo Cara on 28/01/25.
+//
+
+import SwiftUI
+
+public extension View {
+    // blurredSheet
+    func blurredSheet<Content: View> (_ style: AnyShapeStyle, show: Binding<Bool>, onDismiss: @escaping () -> (), @ViewBuilder content: @escaping () -> Content) -> some View {
+        self.sheet(isPresented: show, onDismiss: onDismiss) {
+            content()
+                .background(RemoveBackgroundColor())
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Rectangle()
+                        .fill(style)
+                        .ignoresSafeArea(.container, edges: .all)
+                )
+        }
+    }
+    
+    //persistentBlurredBottomSheet
+    func persistentBlurredBottomSheetfunc<Content: View> (_ style: AnyShapeStyle,initialHeight: CGFloat, presentationDetents: Set<PresentationDetent>? = nil, sheetCornerRadius: CGFloat? = nil , showDragIndicator: Visibility? ,@ViewBuilder content: @escaping () -> Content) -> some View {
+        self
+            .sheet(isPresented: .constant(true), onDismiss: {}) {
+                content()
+                    .background(RemoveBackgroundColor())
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(
+                        Rectangle()
+                            .fill(style)
+                            .ignoresSafeArea(.container, edges: .all)
+                    )
+                    .presentationDetents(presentationDetents ?? [.height(initialHeight)])
+                    .presentationCornerRadius(sheetCornerRadius)
+                    .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+                    .interactiveDismissDisabled()
+                    .presentationDragIndicator(showDragIndicator ?? .automatic)
+            }
+    }
+    
+}
+
+
+fileprivate struct RemoveBackgroundColor: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIView {
+        return UIView()
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        DispatchQueue.main.async {
+            uiView.superview?.superview?.backgroundColor = .clear
+        }
+    }
+    
+}
+
